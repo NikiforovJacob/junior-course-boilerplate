30933cb1cd018afe49b9f99a2cb5cc88d54ddc43
Jacob Nikiforov <nikiforov.jacob@yandex.ru>
2019-08-15 13:59:10 +0700

    Task #3(State snd uncontrolled controls)_Corrections on the mentors comments #2 (after discussion in slack)

--- src/Containers/Filter/Filter.js
+++ src/Containers/Filter/Filter.js
@@ -1,71 +1,44 @@
 import React from 'react';
-import LogRender from '../../Components/logRender/logRender';
 
 import './Filter.css';
 
-class Filter extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.inputPriceFrom = React.createRef();
-    this.inputPriceTo = React.createRef();
+const Filter = (props) => {
 
-    this.filterDefaultValueTo = this.props.data.reduce(
-      (maxPrice, item) => maxPrice > item.price ? maxPrice : item.price
-    , 0)
-      
-    this.filterDelaultValueFrom = this.props.data.reduce(
-      (minPrice, item) => minPrice < item.price ? minPrice : item.price
-    , undefined);
+  const { 
+    handleChangeInputText, 
+    handleSetFilter,
+    filterValueFrom,
+    filterValueTo
+  } = props;
 
-    this.isFilterValuesValid = (value) => {
-      return RegExp('^(0|[1-9][0-9]*)$').test(value);
-    }
-  }
+  const isInputValueValid = (value) => RegExp('^(0|[1-9][0-9]*)$').test(value);
 
-  handleSetFilter = () => {
-    const { data, handleChangeState } = this.props;
-    const  inputPriceFrom = this.inputPriceFrom.current.value;
-    const  inputPriceTo = this.inputPriceTo.current.value;
-
-    if (!(this.isFilterValuesValid(inputPriceFrom)) || !(this.isFilterValuesValid(inputPriceTo))) {
-      alert('Значения цены в фильтре должны быть 0 или положительным числом');
-      return;
-    }
-
-    const filteredData = data.filter(
-      item => item.price >= inputPriceFrom && item.price <= inputPriceTo
-    );
-    handleChangeState({ productsList: filteredData });
-  }
-
-  render() {
-    return (
-      <div className='filter'>
-        <div className='filterTitle'>Цена</div>
-        <div>
-          <label className='label'>
-            от
-            <input
-              className='fields'
-              ref={this.inputPriceFrom}
-              defaultValue={this.filterDelaultValueFrom}
-            />
-          </label>
-          <label className='label'>
-            до
-            <input 
-              className='fields'
-              ref={this.inputPriceTo}
-              defaultValue={this.filterDefaultValueTo}
-            />
-          </label>
-        </div>
-        <button className='button' onClick={this.handleSetFilter}>Применить</button>
-        <LogRender parentContext={this} />
+  return (
+    <div className='filter'>
+      <div className='filterTitle'>Цена</div>
+      <div>
+        <label className='label'>
+          от
+          <input
+            className='fields'
+            onChange={handleChangeInputText}
+            value={filterValueFrom}
+            data-name-of-input='filterValueFrom'
+          />
+        </label>
+        <label className='label'>
+          до
+          <input 
+            className='fields'
+            onChange={handleChangeInputText}
+            value={filterValueTo}
+            data-name-of-input='filterValueTo' 
+          />
+        </label>
       </div>
-    )
-  }
+      <button className='button' onClick={handleSetFilter(isInputValueValid)}>Применить</button>
+    </div>
+  )
 }
 
 export default Filter;
--- src/index.js
+++ src/index.js
@@ -10,21 +10,48 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.defaultFilterValueTo = productsList.reduce(
+      (maxPrice, item) => maxPrice > item.price ? maxPrice : item.price
+    , 0)
+
+    this.defaultFilterValueFrom = productsList.reduce(
+      (minPrice, item) => minPrice < item.price ? minPrice : item.price
+    , this.filterValueTo);
+
     this.state = {
       productsList: productsList,
+      filterValueFrom: this.defaultFilterValueFrom,
+      filterValueTo: this.defaultFilterValueTo,
     }
   }
 
-  handleChangeState = (newState) => {
-    this.setState(newState);
+  handleChangeInputText = (e) => {
+    const inputName = e.target.dataset.nameOfInput;
+    this.setState({ [inputName]: e.target.value });
+  };
+
+  handleSetFilter = (isInputValueValid) => () => {
+    const { filterValueFrom, filterValueTo } = this.state;
+
+    if (!(isInputValueValid(filterValueFrom)) || !(isInputValueValid(filterValueTo))) {
+      alert('Значения цены в фильтре должны быть 0 или положительным числом');
+      return;
+    }
+
+    const filteredData = productsList.filter(
+      item => item.price >= filterValueFrom && item.price <= filterValueTo
+    );
+    this.setState({ productsList: filteredData });
   }
 
   render() {
     return (
       <div className='page'>
         <Filter 
-          data={productsList}
-          handleChangeState={this.handleChangeState}
+          handleChangeInputText={this.handleChangeInputText}
+          handleSetFilter={this.handleSetFilter}
+          filterValueFrom={this.state.filterValueFrom}
+          filterValueTo={this.state.filterValueTo}
         />
         <div className='goods'>
           <Title title='Список товаров' />
@@ -34,7 +61,6 @@ class App extends React.Component {
       </div>
     );
   }
-
 };
 
 const rootElement = document.getElementById("root");
